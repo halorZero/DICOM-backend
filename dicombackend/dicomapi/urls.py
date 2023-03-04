e1394588dedd3db76ec82ae450ce4c60f2849478
庄伟光 <1454101799@qq.com>
2023-03-04 20:12:28 +0800

    上传图片接口

--- dicombackend/dicomapi/urls.py
+++ dicombackend/dicomapi/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    #path('upload/file', upload_file),
-    path('dicom/',dicom),
+    path('upload/', upload),
+    path('dicom/', dicom),
 ]
--- dicombackend/dicomapi/views.py
+++ dicombackend/dicomapi/views.py
@@ -3,22 +3,46 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 import os
+import datetime
+import shutil
 from django.conf import settings
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(settings.BASE_DIR, 'media')
 
 @csrf_exempt
-def dicom(x):
-    file = open('./media/image-000001.dcm','rb')
+def dicom(request, url=os.path.join(MEDIA_ROOT, 'image-000001.dcm')):
+    file = open(url, 'rb')
     res = FileResponse(file)
     res['Content-Type'] = 'application/octet-stream'
     res['Content-Disposition'] = 'attachment;filename="1.dcm"'
     return res
-# Create your views here.
 
-class File(View):
-    def get(self, request, *args, **kwargs):
-        response = HttpResponse("http://127.0.0.1:8000/" + MEDIA_URL + "image-000001.dcm")
-        response['Access-Control-Allow-Origin'] = 'http://localhost:8080'
-        return response
+
+def upload(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        img1 = request.FILES.get('file1')
+        img2 = request.FILES.get('file2')
+
+        date_name = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f_') + str(name)
+        print(date_name)
+
+        img_name1 = date_name + '1.png'
+        img_name2 = date_name + '2.png'
+        f1 = open(os.path.join(MEDIA_ROOT, 'ximg', img_name1), 'wb')
+        f2 = open(os.path.join(MEDIA_ROOT, 'ximg', img_name2), 'wb')
+        for i in img1.chunks():
+            f1.write(i)
+        f1.close()
+        for i in img2.chunks():
+            f2.write(i)
+        f2.close()
+
+        # X 2 DICOM
+        pass
+        dicom_name = date_name + '.dcm'
+        shutil.copyfile(os.path.join(MEDIA_ROOT, 'image-000001.dcm'), os.path.join(MEDIA_ROOT, 'dicom', dicom_name))
+        # X 2 DICOM
+
+        return dicom(request, url=os.path.join(MEDIA_ROOT, 'dicom', dicom_name))
